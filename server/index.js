7247b15ed1965b9bb71c78d43c507f3ce069f9de
Aaron Harris <aaronharris@Aarons-MacBook-Pro.local>
2018-08-10 14:20:06 -0600

    working on reducer

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const massive = require('massive')
+const session = require('express-session')
 require('dotenv').config();
 
 const app = express()
@@ -12,6 +13,11 @@ massive(process.env.CONNECTION_STRING).then( db => {
     app.set('db', db)
     console.log('db is connected!')
 })
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
 
 app.use(bodyParser.json())
 
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,7 +1,11 @@
-// import axios from 'axios'
+import axios from 'axios'
+
 const GET_USERID = 'GET_USERID'
+const GET_USERID_FULFILLED = 'GET_USERID_FULFILLED'
 const GET_USERNAME = 'GET_USERNAME'
+const GET_USERNAME_FULFILLED = 'GET_USERNAME_FULFILLED'
 const GET_PROFILEPICTURE = 'GET_PROFILEPICTURE'
+const GET_PROFILEPICTURE_FULFILLED = 'GET_PROFILEPICTURE_FULFILLED'
 
 let initialState = {
     userName: '',
@@ -12,8 +16,31 @@ let initialState = {
 
 export default function reducer(state = initialState, action) {
     switch (action.type){
-        case GET_USERID:
+        case GET_USERID_FULFILLED:
+        return { ...state, id: action.payload}
+        case GET_USERNAME_FULFILLED:
+        return { ...state, name: action.payload}
+        case GET_PROFILEPICTURE_FULFILLED:
+        return { ...state, profilePicture: action.payload}
         default: 
         return state;
+    }
+}
+export function getUserId (){
+    return{
+        type: GET_USERID,
+        payload: axios.get('/api/userId')
+    }
+}
+export function getUserName (){
+    return {
+        type: GET_USERNAME,
+        payload: axios.get('/api/userName')
+    }
+}
+export function getProfilePicture (){
+    return{
+        type: GET_PROFILEPICTURE,
+        payload: axios.get('/api/profilePicture')
     }
 }
